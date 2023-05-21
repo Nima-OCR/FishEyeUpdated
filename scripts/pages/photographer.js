7c4551977a15ca012a33858e5b909ac7ee78a078
CyrusLeGrand <cyrus.achemenide@gmail.com>
2023-05-21 17:33:13 +0200

    achecker fontawesome i to span updated

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -169,7 +169,7 @@
     function displayRateAndLikes(selectedPhotographer, photographerMediaItems) {
       const asideElement = createElementWithClass("aside", "");
       const rateElement = createRateAndLikesElement("div", "rate", `${selectedPhotographer.price}€ / jour`);
-      const likeElement = createRateAndLikesElement("div", "likes", `<i class="fa fa-heart"></i> `);
+      const likeElement = createRateAndLikesElement("div", "likes", `<span class="fa fa-heart"></span> `);
       const totalLikesElement = createRateAndLikesElement("p", "total-likes", `${calculateTotalLikes(photographerMediaItems)}`);
 
       asideElement.append(rateElement, likeElement, totalLikesElement);
